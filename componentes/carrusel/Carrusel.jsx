95ac46b2e4e914da3c2f6d145f600a22b21d6217
daviddedio <ddedio@gmail.com>
2025-03-30 12:05:19 -0300

    Comenzamos a estilar

---
+++ componentes/carrusel/Carrusel.jsx
@@ -0,0 +1,18 @@
+import './Carrusel.css'
+export const Carrusel = () => {
+    return( 
+        <div className='slider'>
+        <ul>
+            <li>
+                <img src="/Ganador1.jpg" alt="" />
+            </li>
+            <li>
+                <img src="/Ganador2.jpg" alt="" />
+            </li>
+            <li>
+                <img src="/Ganador3.jpg" alt="" />
+            </li>
+        </ul>
+        </div>
+    )
+}
--- src/App.jsx
+++ src/App.jsx
@@ -6,21 +6,27 @@ import { Estudios } from '../routes/Estudios'
 import { Experiencia } from '../routes/Experiencia'
 import { InformacionPage } from '../pages/informacion/InformacionPage'
 import { Modal } from '../componentes/modal/Modal'
-import {UpdateInformacion} from '../routes/UpdateInformacion'
+import { UpdateInformacion } from '../routes/UpdateInformacion'
 import { Context } from '../context/Context'
 import './App.css'
 import { useContext } from 'react'
 
 
 export const App = () => {
-  const {login} = useContext(Context)
+  const { login } = useContext(Context)
   return (
     <>
+      <div className="hero-image">
+        <div className="hero-text">
+          <h1 className='h1style'>CV Virtual</h1>
+          <p>Lic. De Dio, David Emanuel</p>
+        </div>
+      </div>
       <NavBarCv />
       <div className='bigContainer'>
-      <div className="sidebar" >
-        <InformacionPage />
-      </div>
+        <div className="sidebar" >
+          <InformacionPage />
+        </div>
         <div className="body" >
           <Routes className="body" >
             <Route path='/conocimientos' element={<Conocimiento />} />
@@ -29,11 +35,14 @@ export const App = () => {
             <Route path='/experiencia' element={<Experiencia />} />
             <Route path='/' element={<Estudios />} />
             <Route path='/*' element={<Navigate to='/estudios' />} />
-            <Route path='/CargaDatos' element={login ? <UpdateInformacion/> : <Navigate to='/estudios'/>} />
+            <Route path='/CargaDatos' element={login ? <UpdateInformacion /> : <Navigate to='/estudios' />} />
           </Routes>
         </div>
       </div>
       <Modal />
+      <div className="footer-img">
+        Footer
+      </div>
     </>
   )
 }
